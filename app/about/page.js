609d0485debcca2fc8c86a121195966ab201857a
DinDenz <DinDenz@users.noreply.github.com>
2024-04-09 13:43:13 +0300

    generate meta

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "О нас",
+  description: "Страница о нас",
+  //любые метатеги
+};
 
 
 const About = () => {
--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,5 +1,13 @@
 import Post from "@/app/components/Post";
 
+export async function generateMetadata({ params, searchParams }) {
+  const post = await getPosts(params.id);
+  return {
+    title: post.title + " - статья на сайте",
+    description: post.body
+  };
+}
+
 async function getPosts(id) {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
   const data = await res.json();
